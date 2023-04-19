e65a3753a098f8e8e2086ab078cefa15001966f3
Guido <guido.meijer@donders.ru.nl>
2023-04-19 12:33:06 +0200

    Update

--- WIP/plot_states_awake.py
+++ WIP/plot_states_awake.py
@@ -81,9 +81,26 @@ axs = np.concatenate(axs)
 for i, region in enumerate(np.unique(p_state_df['region'])):
 
     axs[i].add_patch(Rectangle((0, -4), 1, 5, color='royalblue', alpha=0.25, lw=0))
+    sns.lineplot(data=state_trans_df[state_trans_df['region'] == region], x='time', y='p_trans',
+                 color='k', errorbar='se', ax=axs[i], err_kws={'lw': 0})
+    axs[i].set(title=region, ylim=[0.05, 0.3], yticks=[0.1], xticks=[-1, 0, 1, 2, 3, 4],
+               ylabel='', xlabel='')
+axs[-1].axis('off')
+f.text(0.5, 0.04, 'Time relative to stimulation onset (s)', ha='center')
+f.text(0.04, 0.5, 'P(state change) over baseline', va='center', rotation='vertical')
+plt.tight_layout(rect=(0.05, 0.05, 1, 1))
+sns.despine(trim=True)
+plt.savefig(join(fig_path, 'state_change_rate.jpg'), dpi=600)
+
+# %%
+f, axs = plt.subplots(2, 4, figsize=(5.25, 2.5), dpi=dpi, sharey=True, sharex=True)
+axs = np.concatenate(axs)
+for i, region in enumerate(np.unique(p_state_df['region'])):
+
+    axs[i].add_patch(Rectangle((0, -4), 1, 5, color='royalblue', alpha=0.25, lw=0))
     sns.lineplot(data=state_trans_df[state_trans_df['region'] == region], x='time', y='p_trans_bl',
                  color='k', errorbar='se', ax=axs[i], err_kws={'lw': 0})
-    axs[i].set(title=region, ylim=[-0.05, 0.08], yticks=[-0.05, 0, 0.05], xticks=[-1, 0, 1, 2, 3, 4],
+    axs[i].set(title=region, ylim=[-0.05, 0.055], yticks=[-0.05, 0, 0.05], xticks=[-1, 0, 1, 2, 3, 4],
                ylabel='', xlabel='')
 axs[-1].axis('off')
 f.text(0.5, 0.04, 'Time relative to stimulation onset (s)', ha='center')
