ed5365e415a95f8ddde67fb180e63bf077e6f33b
Guido Meijer <guido.meijer@research.fchampalimaud.org>
2023-05-24 09:17:27 +0200

    Update

--- Figures/figure5/neuron_type_modulation.py
+++ Figures/figure5/neuron_type_modulation.py
@@ -41,11 +41,12 @@ print(f'p = {p:.2f}')
 
 colors, dpi = figure_style()
 
-f, ax1 = plt.subplots(1, 1, figsize=(1.5, 1.75), dpi=dpi)
-sns.violinplot(data=merged_df, x='type', y='mod_index_late', ax=ax1, palette=[colors['NS'], colors['WS']])
-ax1.set(ylabel='Modulation index', xticklabels=['Narrow\nspiking', 'Wide\nspiking'],
-        xlabel='', yticks=[-1, -0.5, 0, 0.5, 1])
+f, ax1 = plt.subplots(1, 1, figsize=(1.2, 1.75), dpi=dpi)
+sns.violinplot(data=merged_df, x='type', y='mod_index_late', ax=ax1,
+               palette=[colors['NS'], colors['WS']], linewidth=0)
+ax1.set(xticks=[], xlabel='', yticks=[-1, 0, 1])
+ax1.set_ylabel('Modulation index', labelpad=0)
 ax1.text(0.5, 1, 'n.s.', ha='center', va='center', fontsize=7)
-sns.despine(trim=True)
+sns.despine(trim=True, bottom=True)
 plt.tight_layout()
 plt.savefig(join(fig_path, 'neuron_type_modulation.pdf'))
