00045a5d5ad761460db1d59333a7f53e68f0b5e1
Guido Meijer <guido.meijer@research.fchampalimaud.org>
2023-06-01 18:26:40 +0200

    Update

--- Figures/figure6/light_modulated_neurons_per_region_task.py
+++ Figures/figure6/light_modulated_neurons_per_region_task.py
@@ -13,9 +13,9 @@ from os.path import join, realpath, dirname, split
 from stim_functions import paths, figure_style, load_subjects, high_level_regions
 
 # Settings
-MIN_NEURONS_POOLED = 5
-MIN_NEURONS_PER_MOUSE = 5
-MIN_MOD_NEURONS = 10
+MIN_NEURONS_POOLED = 1
+MIN_NEURONS_PER_MOUSE = 1
+MIN_MOD_NEURONS = 1
 MIN_REC = 2
 
 # Get paths
@@ -23,7 +23,7 @@ f_path, save_path = paths()
 fig_path = join(f_path, split(dirname(realpath(__file__)))[-1])
 
 # Load in results
-light_neurons = pd.read_csv(join(save_path, 'light_modulated_neurons_anesthesia.csv'))
+light_neurons = pd.read_csv(join(save_path, 'task_modulated_neurons.csv'))
 
 # Add expression
 subjects = load_subjects()
@@ -38,32 +38,23 @@ light_neurons = light_neurons.reset_index(drop=True)
 light_neurons = light_neurons.drop(index=[i for i, j in enumerate(light_neurons['high_level_region']) if 'root' in j])
 
 # Get modulated neurons
-mod_neurons = light_neurons[(light_neurons['sert-cre'] == 1) & (light_neurons['modulated'] == 1)]
+mod_neurons = light_neurons[(light_neurons['sert-cre'] == 1) & (light_neurons['opto_modulated'] == 1)]
 mod_neurons = mod_neurons.groupby('high_level_region').filter(lambda x: len(x) >= MIN_MOD_NEURONS)
-
-# Add enhanced and suppressed
-light_neurons['enhanced_late'] = light_neurons['modulated'] & (light_neurons['mod_index_late'] > 0)
-light_neurons['suppressed_late'] = light_neurons['modulated'] & (light_neurons['mod_index_late'] < 0)
-light_neurons['enhanced_early'] = light_neurons['modulated'] & (light_neurons['mod_index_early'] > 0)
-light_neurons['suppressed_early'] = light_neurons['modulated'] & (light_neurons['mod_index_early'] < 0)
 
 # Calculate summary statistics
 summary_df = light_neurons[light_neurons['sert-cre'] == 1].groupby(['high_level_region']).sum()
 summary_df['n_neurons'] = light_neurons[light_neurons['sert-cre'] == 1].groupby(['high_level_region']).size()
 summary_df['modulation_index'] = light_neurons[light_neurons['sert-cre'] == 1].groupby(
-    ['high_level_region']).mean(numeric_only=True)['mod_index_late']
+    ['high_level_region']).mean(numeric_only=True)['opto_mod_roc']
 summary_df = summary_df.reset_index()
-summary_df['perc_enh_late'] =  (summary_df['enhanced_late'] / summary_df['n_neurons']) * 100
-summary_df['perc_supp_late'] =  (summary_df['suppressed_late'] / summary_df['n_neurons']) * 100
-summary_df['perc_mod'] =  (summary_df['modulated'] / summary_df['n_neurons']) * 100
-summary_df = summary_df[summary_df['modulated'] >= MIN_MOD_NEURONS]
-summary_df['perc_supp_late'] = -summary_df['perc_supp_late']
+summary_df['perc_mod'] =  (summary_df['opto_modulated'] / summary_df['n_neurons']) * 100
+summary_df = summary_df[summary_df['opto_modulated'] >= MIN_MOD_NEURONS]
 
 summary_no_df = light_neurons[light_neurons['sert-cre'] == 0].groupby(['high_level_region']).sum()
 summary_no_df['n_neurons'] = light_neurons[light_neurons['sert-cre'] == 0].groupby(['high_level_region']).size()
 summary_no_df = summary_no_df.reset_index()
-summary_no_df['perc_mod'] =  (summary_no_df['modulated'] / summary_no_df['n_neurons']) * 100
-summary_no_df = summary_no_df[summary_no_df['modulated'] >= MIN_MOD_NEURONS]
+summary_no_df['perc_mod'] =  (summary_no_df['opto_modulated'] / summary_no_df['n_neurons']) * 100
+summary_no_df = summary_no_df[summary_no_df['opto_modulated'] >= MIN_MOD_NEURONS]
 summary_no_df = pd.concat((summary_no_df, pd.DataFrame(data={
     'high_level_region': summary_df.loc[~summary_df['high_level_region'].isin(summary_no_df['high_level_region']), 'high_level_region'],
     'perc_mod': np.zeros(np.sum(~summary_df['high_level_region'].isin(summary_no_df['high_level_region'])))})))
@@ -74,10 +65,10 @@ ordered_regions = summary_df.sort_values('perc_mod', ascending=False).reset_inde
 # Summary statistics per mouse
 per_mouse_df = light_neurons[light_neurons['sert-cre'] == 1].groupby(
     ['high_level_region', 'subject']).sum(numeric_only=True)
-per_mouse_df['mod_index_late'] = light_neurons[light_neurons['sert-cre'] == 1].groupby(
-    ['high_level_region', 'subject']).median(numeric_only=True)['mod_index_late']
+per_mouse_df['opto_mod_roc'] = light_neurons[light_neurons['sert-cre'] == 1].groupby(
+    ['high_level_region', 'subject']).median(numeric_only=True)['opto_mod_roc']
 per_mouse_df['n_neurons'] = light_neurons[light_neurons['sert-cre'] == 1].groupby(['high_level_region', 'subject']).size()
-per_mouse_df['perc_mod'] = (per_mouse_df['modulated'] / per_mouse_df['n_neurons']) * 100
+per_mouse_df['perc_mod'] = (per_mouse_df['opto_modulated'] / per_mouse_df['n_neurons']) * 100
 per_mouse_df = per_mouse_df[per_mouse_df['n_neurons'] >= MIN_NEURONS_PER_MOUSE]
 per_mouse_df = per_mouse_df.groupby('high_level_region').filter(lambda x: len(x) >= MIN_REC)
 per_mouse_df = per_mouse_df.reset_index()
@@ -114,13 +105,13 @@ plt.savefig(join(fig_path, 'perc_light_modulated_neurons_per_region.pdf'))
 
 PROPS = {'boxprops':{'facecolor':'none', 'edgecolor':'none'}, 'medianprops':{'color':'none'},
          'whiskerprops':{'color':'none'}, 'capprops':{'color':'none'}}
-ORDER = mod_neurons.groupby('high_level_region').mean(numeric_only=True)['mod_index_late'].sort_values(
+ORDER = mod_neurons.groupby('high_level_region').mean(numeric_only=True)['opto_mod_roc'].sort_values(
     ascending=False).reset_index()['high_level_region']
 
 f, ax1 = plt.subplots(1, 1, figsize=(2.5, 1.75), dpi=dpi)
-sns.stripplot(x='mod_index_late', y='high_level_region', ax=ax1, data=mod_neurons, order=ORDER,
+sns.stripplot(x='opto_mod_roc', y='high_level_region', ax=ax1, data=mod_neurons, order=ORDER,
               size=2, color='grey', zorder=1)
-sns.boxplot(x='mod_index_late', y='high_level_region', ax=ax1, data=mod_neurons, showmeans=True,
+sns.boxplot(x='opto_mod_roc', y='high_level_region', ax=ax1, data=mod_neurons, showmeans=True,
             order=ORDER, meanprops={"marker": "|", "markeredgecolor": "red", "markersize": "8"},
             fliersize=0, zorder=2, **PROPS)
 ax1.plot([0, 0], ax1.get_ylim(), ls='--', color='black', zorder=0)
