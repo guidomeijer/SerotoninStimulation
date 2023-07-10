f14f68bb35cf04eec359603f3442cfd125336509
Guido <guido.meijer@donders.ru.nl>
2023-07-10 12:35:21 +0200

    Pupil

---
+++ WIP/pupil_opto_stim_passive.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 13 11:42:01 2021
+
+@author: guido
+"""
+
+import numpy as np
+from os.path import join
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+import seaborn as sns
+from stim_functions import (paths, figure_style, load_passive_opto_times, load_subjects,
+                            query_ephys_sessions, init_one, get_dlc_XYs, get_raw_smooth_pupil_diameter)
+one = init_one()
+
+# Settings
+OVERWRITE = True
+TIME_BINS = np.arange(-0.5, 4.1, 0.1)
+BIN_SIZE = 0.1  # seconds
+BASELINE = [0.5, 0]  # seconds
+fig_path, save_path = paths()
+fig_path = join(fig_path, 'Extra plots', 'Pupil')
+
+# Query and load data
+subjects = load_subjects()
+
+# Query ephys sessions
+rec = query_ephys_sessions(one=one)
+
+if OVERWRITE:
+    results_df = pd.DataFrame()
+else:
+    results_df = pd.read_csv(join(save_path, 'pupil_passive.csv'))
+
+for k, nickname in enumerate(np.unique(rec['subject'])):
+    print(f'Processing {nickname}..')
+
+    # Get eids
+    eids = np.unique(rec.loc[rec['subject'] == nickname, 'eid'])
+
+    pupil_size = pd.DataFrame()
+    for i, eid in enumerate(eids):
+
+        # Skip if session is already done
+        if not OVERWRITE:
+            if eid in results_df['eid']:
+                print('Already processed, skipping')
+                continue
+
+        # Get session details
+        ses_details = one.get_details(eid)
+        date = ses_details['start_time'][:10]
+        if nickname not in subjects['subject'].values:
+            continue
+        expression = subjects.loc[subjects['subject'] == nickname, 'sert-cre'].values[0]
+        print(f'Starting {nickname}, {date}')
+
+        # Load in laser pulse times
+        try:
+            opto_train_times, _ = load_passive_opto_times(eid, one=one)
+        except:
+            continue
+
+        # Load in camera timestamps and DLC output
+        try:
+            video_times, XYs = get_dlc_XYs(one, eid)
+        except:
+            print('Could not load video and/or DLC data')
+            continue
+        if video_times is None:
+            continue
+
+        # If the difference between timestamps and video frames is too large, skip
+        if np.abs(video_times.shape[0] - XYs['pupil_left_r'].shape[0]) > 10000:
+            print('Timestamp mismatch, skipping..')
+            continue
+
+        # Get pupil diameter
+        print('Calculating smoothed pupil trace')
+        raw_diameter, diameter = get_raw_smooth_pupil_diameter(XYs)
+
+        # Assume frames were dropped at the end
+        if video_times.shape[0] > diameter.shape[0]:
+            video_times = video_times[:diameter.shape[0]]
+        elif diameter.shape[0] > video_times.shape[0]:
+            diameter = diameter[:video_times.shape[0]]
+
+        # Calculate percentage change
+        diameter_perc = ((diameter - np.percentile(diameter[~np.isnan(diameter)], 2))
+                         / np.percentile(diameter[~np.isnan(diameter)], 2)) * 100
+
+        # Get trial triggered baseline subtracted pupil diameter
+        for t, trial_start in enumerate(opto_train_times):
+            this_diameter = np.array([np.nan] * TIME_BINS.shape[0])
+            baseline_subtracted = np.array([np.nan] * TIME_BINS.shape[0])
+            baseline = np.nanmean(diameter_perc[(video_times > (trial_start - BASELINE[0]))
+                                                  & (video_times < (trial_start - BASELINE[1]))])
+            for b, time_bin in enumerate(TIME_BINS):
+                this_diameter[b] = np.nanmean(diameter_perc[
+                    (video_times > (trial_start + time_bin) - (BIN_SIZE / 2))
+                    & (video_times < (trial_start + time_bin) + (BIN_SIZE / 2))])
+                baseline_subtracted[b] = np.nanmean(diameter_perc[
+                    (video_times > (trial_start + time_bin) - (BIN_SIZE / 2))
+                    & (video_times < (trial_start + time_bin) + (BIN_SIZE / 2))]) - baseline
+            pupil_size = pd.concat((pupil_size, pd.DataFrame(data={
+                'diameter': this_diameter, 'baseline_subtracted': baseline_subtracted, 'eid': eid,
+                'subject': nickname, 'trial': t, 'time': TIME_BINS, 'expression': expression,
+                'date': date})))
+
+        # Add to overal dataframe
+        results_df = pd.concat((results_df, pd.DataFrame(data={
+            'diameter': pupil_size[pupil_size['eid'] == eid].groupby('time').mean(numeric_only=True)['diameter'],
+            'baseline_subtracted': pupil_size[pupil_size['eid'] == eid].groupby('time').mean(numeric_only=True)['baseline_subtracted'],
+            'time': TIME_BINS, 'subject': nickname, 'expression': expression, 'eid': eid})), ignore_index=True)
+
+    # Save output
+    results_df.to_csv(join(save_path, 'pupil_passive.csv'))
+
+    # Plot this animal
+    colors, dpi = figure_style()
+    pupil_size = pupil_size.reset_index(drop=True)
+
+    f, (ax1, ax2) = plt.subplots(1, 2, figsize=(4, 2), dpi=dpi)
+    sns.lineplot(x='time', y='diameter', estimator=np.nanmean, data=pupil_size,
+                 color='k', errorbar='se', ax=ax1, legend=None)
+    ylim = ax1.get_ylim()
+    ax1.add_patch(Rectangle((0, 0), 1, 500, color='royalblue', alpha=0.25, lw=0))
+    ax1.set(title='%s, expression: %d' % (nickname, expression),
+            ylabel='Pupil size (%)', xlabel='Time (s)', ylim=ylim,
+            xticks=np.arange(-1, TIME_BINS[-1]+0.1))
+
+    sns.lineplot(x='time', y='baseline_subtracted', estimator=np.nanmean, data=pupil_size,
+                 color='k', errorbar='se', ax=ax2, legend=None)
+    ylim = ax2.get_ylim()
+    ax2.add_patch(Rectangle((0, -100), 1, 200, color='royalblue', alpha=0.25, lw=0))
+    ax2.set(title='%s, expression: %d' % (nickname, expression), ylim=ylim,
+            ylabel='Baseline subtracted\npupil size (%)', xlabel='Time (s)',
+            xticks=np.arange(-1, TIME_BINS[-1]+1))
+
+    plt.tight_layout()
+    sns.despine(trim=True)
+
+    plt.savefig(join(fig_path, f'{nickname}_pupil_opto_passive.jpg'), dpi=600)
+    plt.close(f)
+
+
