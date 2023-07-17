082119732cb196b7c0b14ee23473e07bce82a867
Guido Meijer <guido.meijer@research.fchampalimaud.org>
2023-07-17 10:07:02 +0200

    update

--- WIP/plot_CCA_passive.py
+++ WIP/plot_CCA_passive.py
@@ -13,7 +13,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy.ndimage import gaussian_filter
 from stim_functions import paths, load_subjects, figure_style
-from dlc_functions import smooth_interpolate_signal_sg
 
 # Settings
 
@@ -29,22 +28,42 @@ subjects = load_subjects()
 for i, nickname in enumerate(np.unique(subjects['subject'])):
     cca_df.loc[cca_df['subject'] == nickname, 'sert-cre'] = subjects.loc[subjects['subject'] == nickname, 'sert-cre'].values[0]
 
-# Select sert mice and regions of interest
-cca_df = cca_df[cca_df['sert-cre'] == 1].reset_index()
-
 # Get time axis
 time_ax = np.round(cca_df['time'].mean(), 3)
 
+# Create long dataframe
+cca_long_df = pd.DataFrame()
+for i in cca_df.index:
+    region1 = cca_df.loc[i, 'region_1']
+    region2 = cca_df.loc[i, 'region_2']
+    cca_long_df = pd.concat((cca_long_df, pd.DataFrame(data={
+        'time': time_ax, 'r': cca_df.loc[i, 'r_mean'],
+        'r_bl': cca_df.loc[i, 'r_mean'] - np.mean(cca_df.loc[i, 'r_mean'][time_ax < 0]),
+        'subject': cca_df.loc[i, 'subject'],
+        'date': cca_df.loc[i, 'date'], 'eid': cca_df.loc[i, 'eid'], 
+        'sert-cre': cca_df.loc[i, 'sert-cre'],
+        'region_pair': f'{np.sort([region1, region2])[0]}-{np.sort([region1, region2])[1]}'})))
+
+
 # %%
 colors, dpi = figure_style()
-f, axs = plt.subplots(2, 5, figsize=(7, 3.5), dpi=dpi)
-axs = np.concatenate(axs)
-for i in cca_df.index:
-    axs[i].fill_between(cca_df.loc[i, 'time'], cca_df.loc[i, 'r_mean'] - cca_df.loc[i, 'r_std'],
-                        cca_df.loc[i, 'r_mean'] + cca_df.loc[i, 'r_std'], alpha=0.2)
-    axs[i].plot(cca_df.loc[i, 'time'], cca_df.loc[i, 'r_mean'])
-    axs[i].set(ylabel='Canonical correlation (r)', xlabel='Time (s)', xticks=[-1, 0, 1, 2, 3],
-            title=f'{cca_df.loc[i, "subject"]} {cca_df.loc[i, "region_1"]}-{cca_df.loc[i, "region_2"]}')
+
+f, ax1 = plt.subplots(1, 1, figsize=(1.75, 1.75), dpi=dpi)
+ax1.add_patch(Rectangle((0, -0.2), 1, 0.8, color='royalblue', alpha=0.25, lw=0))
+sns.lineplot(data=cca_long_df, x='time', y='r_bl', color='k', errorbar='se', ax=ax1,
+             err_kws={'lw': 0}, hue='sert-cre', hue_order=[0, 1], palette=[colors['wt'], colors['sert']])
+g = ax1.legend(title='', bbox_to_anchor=(0.6, 0.85), prop={'size': 5})
+new_labels = ['label 1', 'label 2']
+for t, l in zip(g.texts, ['WT', 'SERT']):
+    t.set_text(l)
+ax1.set(ylabel='Canonical correlation (r)', xlabel='Time (s)', xticks=[-1, 0, 1, 2, 3],
+        ylim=[-0.2, 0.2])
 
 sns.despine(trim=True)
 plt.tight_layout()
+
+# %% Plot region pairs seperately
+g = sns.FacetGrid(cca_long_df[cca_long_df['sert-cre'] == 1], col='region_pair', col_wrap=5, height=3.5,
+                  ylim=(-0.2, 0.8))
+g.map(sns.lineplot, 'time', 'r', color='k', errorbar='se')
+#plt.savefig(join(fig_path, 'state_correlation_all_region_pair.jpg'), dpi=600)
