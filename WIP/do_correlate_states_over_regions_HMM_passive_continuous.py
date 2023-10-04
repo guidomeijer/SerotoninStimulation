8430594d96b620a66b39a5beec68bfca7bf73e1b
Guido <guido.meijer@research.fchampalimaud.org>
2023-10-04 23:18:11 +0200

    .

--- WIP/do_correlate_states_over_regions_HMM_passive_continuous.py
+++ WIP/do_correlate_states_over_regions_HMM_passive_continuous.py
@@ -13,30 +13,32 @@ from itertools import permutations
 from scipy.stats import pearsonr
 from os.path import join, split
 import matplotlib.pyplot as plt
-from stim_functions import paths, figure_style
+from stim_functions import paths, figure_style, init_one, load_passive_opto_times
+one = init_one()
 
 # Settings
 CMAP = 'Set2'
-RANDOM_TIMES = 'jitter'  # spont (spontaneous) or jitter (jittered times during stim period)
+RANDOM_TIMES = 'spont'  # spont (spontaneous) or jitter (jittered times during stim period)
 PRE_TIME = 1
 POST_TIME = 4
-PLOT = False
+PLOT = True
 ORIG_BIN_SIZE = 0.1  # original bin size
 BIN_SIZE = 0.3  # binning to apply for this analysis
 BIN_SHIFT = 0.1
 PRE_TIME = 1
 POST_TIME = 4
 N_STATES_SELECT = 'global'
+RANDOM_TIMES = 'jitter'
 
 # Plotting
 colors, dpi = figure_style()
 
 # Get paths
 f_path, save_path = paths()
-fig_path = join(f_path, 'Extra plots', 'State', 'Correlation matrices', f'{RANDOM_TIMES}')
+fig_path = join(f_path, 'Extra plots', 'State', 'Correlation matrices', f'continuous_{RANDOM_TIMES}')
 
 # Get all files
-all_files = glob(join(save_path, 'HMM', 'PassiveEvent', f'{RANDOM_TIMES}', 'prob_mat', '*.npy'))
+all_files = glob(join(save_path, 'HMM', 'PassiveContinuous', '*prob.npy'))
 
 # Get all recordings
 all_rec = np.unique([split(i)[1][:20] for i in all_files])
@@ -50,17 +52,43 @@ corr_df = pd.DataFrame()
 for i, this_rec in enumerate(all_rec):
     print(f'Processing recording {i} of {len(all_rec)}')
 
+    # Get session info
+    subject = this_rec[:9]
+    date = this_rec[10:20]
+    eid = one.search(subject=subject, date_range=date)[0]
+    
+    # Get opto onset times and random times
+    opto_times, _ = load_passive_opto_times(eid, one=one)
+    if RANDOM_TIMES == 'jitter':
+        random_times = np.sort(np.random.uniform(opto_times[0]-PRE_TIME, opto_times[-1]+POST_TIME,
+                                                 size=opto_times.shape[0]))
+    elif RANDOM_TIMES == 'spont':
+        random_times = np.sort(np.random.uniform(opto_times[0]-360, opto_times[0]-10,
+                                                 size=opto_times.shape[0]))
+        
+    # Skip long opto sessions for now
+    if np.mean(np.diff(opto_times)) > 20:
+        continue
+
     # Get all brain regions simultaneously recorded in this recording session
     rec_region_paths = glob(
-        join(save_path, 'HMM', 'PassiveEvent', f'{RANDOM_TIMES}', 'prob_mat', f'{this_rec[:20]}*'))
+        join(save_path, 'HMM', 'PassiveContinuous', f'{this_rec}*prob.npy'))
     rec_region = dict()
-    for ii in range(len(rec_region_paths)):
-        rec_region[split(rec_region_paths[ii])[1][29:-4]] = np.load(join(rec_region_paths[ii]))
+    for ii, this_region in enumerate(rec_region_paths):
+        
+        # Load in HMM output
+        states = np.load(this_region)
+        time_ax = np.load(this_region[:-8] + 'time.npy')
+        rel_time = np.arange(-PRE_TIME + ORIG_BIN_SIZE/2, (POST_TIME - ORIG_BIN_SIZE/2) + ORIG_BIN_SIZE,
+                             ORIG_BIN_SIZE)
 
-    # Get recording data
-    subject = split(rec_region_paths[0])[1][:9]
-    date = split(rec_region_paths[0])[1][10:20]
-    probe = split(rec_region_paths[0])[1][21:28]
+        prob_mat = []
+        for t, this_time in enumerate(opto_times):
+            prob_mat.append(states[(time_ax >= this_time - PRE_TIME)
+                                   & (time_ax <= this_time + POST_TIME)])
+        prob_mat = np.transpose(np.dstack(prob_mat), (2, 0, 1))
+        
+        rec_region[split(rec_region_paths[ii])[1][29:-9]] = prob_mat
 
     # Correlate each area with each other area
     for r1, region1 in enumerate(rec_region.keys()):
@@ -83,14 +111,12 @@ for i, this_rec in enumerate(all_rec):
                     for state2 in range(rec_region[region2].shape[2]):
 
                         # Bin state probabilities
-                        r1s1 = np.mean(rec_region[region1][
-                            rec_region[region1].shape[0]//2:,
-                            (time_ax >= bin_center - BIN_SIZE/2) & (time_ax <= bin_center + BIN_SIZE/2),
+                        r1s1 = np.mean(rec_region[region1][:,
+                            (rel_time >= bin_center - BIN_SIZE/2) & (rel_time <= bin_center + BIN_SIZE/2),
                             state1],
                             axis=1)
-                        r2s2 = np.mean(rec_region[region2][
-                            rec_region[region2].shape[0]//2:,
-                            (time_ax >= bin_center - BIN_SIZE/2) & (time_ax <= bin_center + BIN_SIZE/2),
+                        r2s2 = np.mean(rec_region[region2][:,
+                            (rel_time >= bin_center - BIN_SIZE/2) & (rel_time <= bin_center + BIN_SIZE/2),
                             state2],
                             axis=1)
 
@@ -110,7 +136,7 @@ for i, this_rec in enumerate(all_rec):
                 'time': bin_centers, 'r_mean': corr_mean, 'r_max': corr_max, 'r_min': corr_min,
                 'region1': region1, 'region2': region2, 'opto': 1,
                 'region_pair': f'{np.sort([region1, region2])[0]}-{np.sort([region1, region2])[1]}',
-                'subject': subject, 'date': date, 'probe': probe})))
+                'subject': subject, 'date': date})))
 
             # Plot an example correlation matrix
             if PLOT:
@@ -134,12 +160,23 @@ for i, this_rec in enumerate(all_rec):
                 plt.close(f)
 
     # Do the same for the random onset times null condition
-    # Get all brain regions simultaneously recorded in this recording session
-    rec_region_paths = glob(
-        join(save_path, 'HMM', 'PassiveEvent', f'{RANDOM_TIMES}', 'prob_mat', f'{this_rec[:20]}*'))
+    # Get all brain regions simultaneously recorded in this recording session  
     rec_region = dict()
-    for ii in range(len(rec_region_paths)):
-        rec_region[split(rec_region_paths[ii])[1][29:-4]] = np.load(join(rec_region_paths[ii]))
+    for ii, this_region in enumerate(rec_region_paths):
+        
+        # Load in HMM output
+        states = np.load(this_region)
+        time_ax = np.load(this_region[:-8] + 'time.npy')
+        rel_time = np.arange(-PRE_TIME + ORIG_BIN_SIZE/2, (POST_TIME - ORIG_BIN_SIZE/2) + ORIG_BIN_SIZE,
+                             ORIG_BIN_SIZE)
+
+        prob_mat = []
+        for t, this_time in enumerate(random_times):
+            prob_mat.append(states[(time_ax >= this_time - PRE_TIME)
+                                   & (time_ax <= this_time + POST_TIME)])
+        prob_mat = np.transpose(np.dstack(prob_mat), (2, 0, 1))
+        
+        rec_region[split(rec_region_paths[ii])[1][29:-9]] = prob_mat
 
     # Correlate each area with each other area
     for r1, region1 in enumerate(rec_region.keys()):
@@ -162,14 +199,12 @@ for i, this_rec in enumerate(all_rec):
                     for state2 in range(rec_region[region2].shape[2]):
 
                         # Bin state probabilities
-                        r1s1 = np.mean(rec_region[region1][
-                            :rec_region[region1].shape[0]//2,
-                            (time_ax >= bin_center - BIN_SIZE/2) & (time_ax <= bin_center + BIN_SIZE/2),
+                        r1s1 = np.mean(rec_region[region1][:,
+                            (rel_time >= bin_center - BIN_SIZE/2) & (rel_time <= bin_center + BIN_SIZE/2),
                             state1],
                             axis=1)
-                        r2s2 = np.mean(rec_region[region2][
-                            :rec_region[region2].shape[0]//2,
-                            (time_ax >= bin_center - BIN_SIZE/2) & (time_ax <= bin_center + BIN_SIZE/2),
+                        r2s2 = np.mean(rec_region[region2][:,
+                            (rel_time >= bin_center - BIN_SIZE/2) & (rel_time <= bin_center + BIN_SIZE/2),
                             state2],
                             axis=1)
 
@@ -189,7 +224,7 @@ for i, this_rec in enumerate(all_rec):
                 'time': bin_centers, 'r_mean': corr_mean, 'r_max': corr_max, 'r_min': corr_min,
                 'region1': region1, 'region2': region2, 'opto': 0,
                 'region_pair': f'{np.sort([region1, region2])[0]}-{np.sort([region1, region2])[1]}',
-                'subject': subject, 'date': date, 'probe': probe})))
+                'subject': subject, 'date': date})))
 
             # Plot an example correlation matrix
             if PLOT:
@@ -214,4 +249,4 @@ for i, this_rec in enumerate(all_rec):
                 plt.close(f)
 
     # Save output
-    corr_df.to_csv(join(save_path, f'state_correlation_{RANDOM_TIMES}_passive.csv'))
+    corr_df.to_csv(join(save_path, f'state_correlation_{RANDOM_TIMES}_passive_cont.csv'))
