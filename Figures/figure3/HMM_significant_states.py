6601f637f0ab5505e32c0804ca518ad3c2e128f2
guidomeijer <guido.meijer@research.fchampalimaud.org>
2024-08-19 14:10:43 +0200

    .

--- Figures/figure3/HMM_significant_states.py
+++ Figures/figure3/HMM_significant_states.py
@@ -28,6 +28,7 @@ f_path, repo_path = paths()
 fig_path = join(f_path, split(dirname(realpath(__file__)))[-1])
 data_path = join(repo_path, 'HMM', 'PassiveEvent', 'spont')
 rec_files = glob(join(data_path, '*.pickle'))
+state_pupil_df = pd.read_csv(join(repo_path, 'state_pupil_corr_baseline.csv'))
 
 subjects = load_subjects()
 state_sig_df = pd.DataFrame()
@@ -35,6 +36,8 @@ for i, file_path in enumerate(rec_files):
     
     # Get info
     subject = split(file_path)[1][:9]
+    date = split(file_path)[1][10:20]
+    region = split(file_path)[1].split('_')[-1][:-7]
     if subjects.loc[subjects['subject'] == subject, 'sert-cre'].values[0] == 0:
         continue
     
@@ -59,16 +62,23 @@ for i, file_path in enumerate(rec_files):
             state_sign[s] = -1
         else:
             state_sign[s] = 1
-            
+    
+    # Get whether these states were significantly modulated by arousal
+    state_ind = state_pupil_df['file_name'] == split(file_path)[1]
+    if np.sum(state_ind) == 0:
+        continue
+    sig_pupil = state_pupil_df.loc[state_ind, 'p'] < 0.05
+    poscorr_pupil = state_pupil_df.loc[state_ind, 'r'] > 0
+    
     # Add to dataframe
     state_sig_df = pd.concat((state_sig_df, pd.DataFrame(data={
-        'p': p_values, 'sign': state_sign, 'region': region, 'subject': subject})))
+        'sig_state': p_values < 0.05, 'sign': state_sign, 'sig_pupil': sig_pupil,
+        'r_pupil': state_pupil_df.loc[state_ind, 'r'], 'state': np.arange(n_states),
+        'region': region, 'subject': subject})))
+state_sig_df['state_sig_pupil'] = state_sig_df['sig_state'] & state_sig_df['sig_pupil']
 
-# Select significant states
-state_sig_df['sig_state'] = (state_sig_df['p'] < 0.05).astype(int)
-
-# Create over subjects summary
-summary_df = state_sig_df[['sign', 'region', 'subject', 'sig_state']].groupby(
+# %% Create over subjects summary
+summary_df = state_sig_df[['sign', 'region', 'subject', 'sig_state', 'sig_pupil']].groupby(
     ['sign', 'region', 'subject']).sum().reset_index()
 enh_state_df = summary_df[summary_df['sign'] == 1]
 supp_state_df = summary_df[summary_df['sign'] == -1]
@@ -79,6 +89,13 @@ supp_state_df = supp_state_df.groupby('region').filter(lambda x: len(x) >= MIN_R
 # Order
 ordered_regions = enh_state_df.groupby('region').mean(numeric_only=True).sort_values('sig_state', ascending=False).reset_index()
 
+enh_states = state_sig_df[state_sig_df['sig_state'] & (state_sig_df['sign'] == 1)]
+enh_summary_df = enh_states[['region', 'subject', 'sig_pupil']].groupby(
+    ['region', 'subject']).sum().reset_index()
+supp_states = state_sig_df[state_sig_df['sig_state'] & (state_sig_df['sign'] == -1)]
+supp_summary_df = supp_states[['region', 'subject', 'sig_pupil']].groupby(
+    ['region', 'subject']).sum().reset_index()
+supp_summary_df['sig_pupil'] = -supp_summary_df['sig_pupil']
 
 # %% Plot
 
@@ -97,8 +114,46 @@ sns.despine(trim=True)
 plt.tight_layout()
 plt.savefig(join(fig_path, 'n_significant_states.pdf'))
 
+# %%
+
+state_sig_df.loc[state_sig_df['sig_state'] & (state_sig_df['sign'] == 1), 'r_pupil'].mean()
+state_sig_df.loc[state_sig_df['sig_state'] & (state_sig_df['sign'] == -1), 'r_pupil'].mean()
 
 
+state_sig_df.loc[(state_sig_df['r_pupil'] > 0) & state_sig_df['sig_pupil'], 'sig_state'].sum()
+state_sig_df.loc[(state_sig_df['r_pupil'] < 0) & state_sig_df['sig_pupil'], 'sig_state'].sum()
+state_sig_df.loc[state_sig_df['sig_pupil'] == 1, 'sig_state'].sum()
+(state_sig_df.loc[state_sig_df['sig_pupil'] == 0, 'sig_state'].sum()
+ / state_sig_df.loc[state_sig_df['sig_pupil'] == 0, 'sig_state'].size)
+
+
+(state_sig_df.loc[state_sig_df['sig_pupil'] == 1, 'sig_state'].sum()
+ / state_sig_df.loc[state_sig_df['sig_pupil'] == 1, 'sig_state'].size)
+
+
+(state_sig_df.loc[state_sig_df['sig_pupil'] == 0, 'sig_state'].sum()
+ / state_sig_df.loc[state_sig_df['sig_pupil'] == 0, 'sig_state'].size)
+
+(state_sig_df.loc[(state_sig_df['r_pupil'] > 0) & state_sig_df['sig_pupil'] == 1, 'sig_state'].sum()
+ / state_sig_df.loc[(state_sig_df['r_pupil'] > 0) & state_sig_df['sig_pupil'] == 1, 'sig_state'].size)
+
+(state_sig_df.loc[(state_sig_df['r_pupil'] < 0) & state_sig_df['sig_pupil'] == 1, 'sig_state'].sum()
+ / state_sig_df.loc[(state_sig_df['r_pupil'] < 0) & state_sig_df['sig_pupil'] == 1, 'sig_state'].size)
+
+
+f, ax1 = plt.subplots(1, 1, figsize=(2.2, 2), dpi=dpi)
+
+#sns.swarmplot(data=enh_state_df, x='sig_states_count', y='region', ax=ax1, s=1)
+sns.barplot(data=state_sig_df.loc[state_sig_df['sig_state'] & (state_sig_df['sign'] == 1)],
+            x='r_pupil', y='region', errorbar='se', ax=ax1,
+            order=ordered_regions['region'], color=colors['enhanced'], label='Enhanced')
+sns.barplot(data=state_sig_df.loc[state_sig_df['sig_state'] & (state_sig_df['sign'] == -1)],
+            x='r_pupil', y='region', errorbar='se', ax=ax1,
+            order=ordered_regions['region'], color=colors['suppressed'], label='Suppressed')
+
+
+sns.despine(trim=True)
+plt.tight_layout()
 
 
     
