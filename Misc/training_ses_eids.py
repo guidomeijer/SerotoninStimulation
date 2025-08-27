21f9a0926a53a70e65c147d145b9b42db7a24e6b
Guido Meijer <guido.meijer@research.fchampalimaud.org>
2025-08-27 11:51:40 +0200

    .

--- Misc/training_ses_eids.py
+++ Misc/training_ses_eids.py
@@ -5,14 +5,29 @@ Created on Fri Aug 22 11:32:45 2025
 By Guido Meijer
 """
 
-from stim_functions import load_subjects, init_one, query_opto_sessions
 
-one = init_one(open_one=False)
-subjects = load_subjects()
-
+from one.api import ONE
+one = ONE()
+subjects = ['ZFM-02181',
+            'ZFM-02600',
+            'ZFM-02601',
+            'ZFM-03321',
+            'ZFM-03323',
+            'ZFM-03324',
+            'ZFM-03329',
+            'ZFM-03330',
+            'ZFM-03331',
+            'ZFM-03332',
+            'ZFM-04080',
+            'ZFM-04083',
+            'ZFM-04122',
+            'ZFM-04300',
+            'ZFM-04811',
+            'ZFM-05170',
+            'ZFM-04820']
 eids = []
-for i, nickname in enumerate(subjects['subject']):
-        
+for i, nickname in enumerate(subjects):
     these_eids = one.alyx.rest('sessions', 'list', subject=nickname,
                                task_protocol='_iblrig_tasks_opto_biasedChoiceWorld')
     eids.append(these_eids)
+print(eids)
